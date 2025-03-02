1c4a421cafb61e07cb3dfc1d8730e9ab47fe42dc
Aman Bhatt <amanbhatt0910@gmail.com>
2025-03-02 17:46:59 +0530

    Frontenc init done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="h-screen flex items-center justify-center bg-gray-100 text-gray-900">
+        <h1 className="text-3xl font-bold">Secure Delivery System</h1>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
